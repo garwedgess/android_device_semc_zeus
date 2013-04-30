bbbd9e03c9ee281a85a92ac49f5b7c5b8d949c17
garwedgess <garethwilliams21@gmail.com>
2013-04-30 19:41:14 +0100

    zeus:small clean-up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,9 +10,10 @@ TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := gar_zeus_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/zeus/recovery/init.rc
 
-# sensor
+# Sensors
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
@@ -23,7 +24,7 @@ TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
 
 BOARD_USES_STEREO_HW_SPEAKER := true
 
-# assert
+# Assert
 TARGET_OTA_ASSERT_DEVICE := R800i,R800a,R800at,zeus
 
 -include vendor/semc/zeus/BoardConfigVendor.mk
